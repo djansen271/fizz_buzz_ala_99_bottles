46e3e9cce0cc067a8e025233cd2b279c48d16076
Damon Jansen <djansen271@gmail.com>
2021-01-30 14:01:24 -0800

    Handle FizzBuzz case

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -2,7 +2,9 @@ class FizzBuzz
   def single_verse(n)
     n_div_by_3 = n % 3 == 0
     n_div_by_5 = n % 5 == 0
-    if n_div_by_3
+    if n_div_by_3 && n_div_by_5
+      'FizzBuzz'
+    elsif n_div_by_3
       'Fizz'
     elsif n_div_by_5
       'Buzz'
