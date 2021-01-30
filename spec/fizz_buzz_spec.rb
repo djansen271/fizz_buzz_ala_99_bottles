efae3f2de4cbf54f89914ee0b6452d880420b9bc
Damon Jansen <djansen271@gmail.com>
2021-01-30 14:14:37 -0800

    Fix typos in spec

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,7 +1,7 @@
 describe FizzBuzz do
   let(:fizz_response) { 'Fizz' }
   let(:buzz_response) { 'Buzz' }
-  let(:fizzbuzz_respone) { 'FizzBuzz' }
+  let(:fizzbuzz_response) { 'FizzBuzz' }
 
   describe '#single_verse' do
     it 'returns 1 given 1' do
@@ -21,7 +21,7 @@ describe FizzBuzz do
     end
 
     it 'returns FizzBuzz given 15' do
-      expect(subject.single_verse(15)).to eq(fizzbuzz_respone)
+      expect(subject.single_verse(15)).to eq(fizzbuzz_response)
     end
 
     it 'returns 32 given 32' do
@@ -37,7 +37,7 @@ describe FizzBuzz do
     end
 
     it 'returns FizzBuzz given 45' do
-      expect(subject.single_verse(45)).to eq(fizzbuzz_respone)
+      expect(subject.single_verse(45)).to eq(fizzbuzz_response)
     end
   end
 
@@ -53,16 +53,17 @@ describe FizzBuzz do
 
   describe '#song' do
     it 'returns 1,2,Fizz,4,Buzz,6,7,..Fizz,100' do
-      expect(subject.song).to eq(
-        %W( 1  2  #{fizz_response} 4  %{buzz_response} #{fizz_response} 7  8  #{fizz_response} #{buzz_response} 11 #{fizz_response} 13 14 %{fizzbuzz_response}
-            16 17 #{fizz_response} 19 %{buzz_response} #{fizz_response} 22 23 #{fizz_response} #{buzz_response} 26 #{fizz_response} 28 29 %{fizzbuzz_response}
-            31 32 #{fizz_response} 34 %{buzz_response} #{fizz_response} 37 38 #{fizz_response} #{buzz_response} 41 #{fizz_response} 43 44 %{fizzbuzz_response}
-            46 47 #{fizz_response} 49 %{buzz_response} #{fizz_response} 52 53 #{fizz_response} #{buzz_response} 56 #{fizz_response} 58 59 %{fizzbuzz_response}
-            61 62 #{fizz_response} 64 %{buzz_response} #{fizz_response} 67 68 #{fizz_response} #{buzz_response} 71 #{fizz_response} 73 74 %{fizzbuzz_response}
-            76 77 #{fizz_response} 79 %{buzz_response} #{fizz_response} 82 82 #{fizz_response} #{buzz_response} 86 #{fizz_response} 88 89 %{fizzbuzz_response}
-            91 92 #{fizz_response} 94 %{buzz_response} #{fizz_response} 97 98 #{fizz_response} #{buzz_response}
-        ).map(&:strip)
-      )
+      expected = %W(
+      1  2  #{fizz_response} 4  #{buzz_response} #{fizz_response} 7  8  #{fizz_response} #{buzz_response} 11 #{fizz_response} 13 14 #{fizzbuzz_response}
+      16 17 #{fizz_response} 19 #{buzz_response} #{fizz_response} 22 23 #{fizz_response} #{buzz_response} 26 #{fizz_response} 28 29 #{fizzbuzz_response}
+      31 32 #{fizz_response} 34 #{buzz_response} #{fizz_response} 37 38 #{fizz_response} #{buzz_response} 41 #{fizz_response} 43 44 #{fizzbuzz_response}
+      46 47 #{fizz_response} 49 #{buzz_response} #{fizz_response} 52 53 #{fizz_response} #{buzz_response} 56 #{fizz_response} 58 59 #{fizzbuzz_response}
+      61 62 #{fizz_response} 64 #{buzz_response} #{fizz_response} 67 68 #{fizz_response} #{buzz_response} 71 #{fizz_response} 73 74 #{fizzbuzz_response}
+      76 77 #{fizz_response} 79 #{buzz_response} #{fizz_response} 82 82 #{fizz_response} #{buzz_response} 86 #{fizz_response} 88 89 #{fizzbuzz_response}
+      91 92 #{fizz_response} 94 #{buzz_response} #{fizz_response} 97 98 #{fizz_response} #{buzz_response}
+  ).map(&:strip)
+      binding.pry
+      expect(subject.song).to eq(expected)
     end
   end
 
