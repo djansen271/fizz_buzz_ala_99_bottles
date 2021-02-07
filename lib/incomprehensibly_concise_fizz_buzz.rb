ea2b7e0e7fb9c66096ecec69fa4b4068306b169d
Damon Jansen <djansen271@gmail.com>
2021-02-06 17:08:11 -0800

    Add IncomprehensiblyConciseFizzBuzz#single_verse

--- lib/incomprehensibly_concise_fizz_buzz.rb
+++ lib/incomprehensibly_concise_fizz_buzz.rb
@@ -1,5 +1,6 @@
 class IncomprehensiblyConciseFizzBuzz
-  def single_verse(_)
+  def single_verse(n)
+    (n % 15 == 0) ? 'FizzBuzz' : (n % 3 == 0 ? 'Fizz' : (n % 5 == 0 ? 'Buzz' : n.to_s))
   end
 
   def verses(_,_)
