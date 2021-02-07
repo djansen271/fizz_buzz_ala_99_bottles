cbc7d84125163292371cb68f68c668dbeeeb38cf
Damon Jansen <djansen271@gmail.com>
2021-02-06 17:14:25 -0800

    Add IncomprehensiblyConciseFizzBuzz#verses

--- lib/incomprehensibly_concise_fizz_buzz.rb
+++ lib/incomprehensibly_concise_fizz_buzz.rb
@@ -3,7 +3,8 @@ class IncomprehensiblyConciseFizzBuzz
     (n % 15 == 0) ? 'FizzBuzz' : (n % 3 == 0 ? 'Fizz' : (n % 5 == 0 ? 'Buzz' : n.to_s))
   end
 
-  def verses(_,_)
+  def verses(a,b)
+    (a..b).map { |c| single_verse(c) }
   end
 
   def song
