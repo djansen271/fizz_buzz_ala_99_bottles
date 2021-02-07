fe88c9c4cd68e3b99275249d1f8a88b47853a19c
Damon Jansen <djansen271@gmail.com>
2021-02-07 12:00:15 -0800

    Finish ConcretelyAbstractFizzBuzz

--- lib/concretely_abstract_fizz_buzz.rb
+++ lib/concretely_abstract_fizz_buzz.rb
@@ -1,10 +1,37 @@
 class ConcretelyAbstractFizzBuzz
-  def single_verse(_)
+  def single_verse(n)
+    Answer.new(n).to_s
   end
 
-  def verses(_,_)
+  def verses(starting,ending)
+    (starting..ending).map{ |n| single_verse(n) }
   end
 
   def song
+    verses(1,100)
+  end
+end
+
+class Answer
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def to_s
+    fizz + buzz + num
+  end
+
+  def fizz
+    number % 3 == 0 ? 'Fizz' : ''
+  end
+
+  def buzz
+    number % 5 == 0 ? 'Buzz' : ''
+  end
+
+  def num
+    (number % 3 != 0 && number % 5 != 0) ? number.to_s : ''
   end
 end
