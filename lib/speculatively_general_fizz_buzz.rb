f54af7ce43cbe68d2787b2464c2441d0646cf717
Damon Jansen <djansen271@gmail.com>
2021-02-07 10:57:04 -0800

    Implement SpeculativelyGeneralFizzBuzz#single_verse

--- lib/speculatively_general_fizz_buzz.rb
+++ lib/speculatively_general_fizz_buzz.rb
@@ -1,10 +1,42 @@
 class SpeculativelyGeneralFizzBuzz
-  def single_verse(_)
+  Fizz = lambda { |n| 'Fizz' }
+  Buzz = lambda { |n| 'Buzz' }
+  FizzBuzz = lambda { |n| 'FizzBuzz' }
+  Num = lambda { |n| n.to_s }
+
+  def single_verse(n)
+    verse_for(n).text
   end
 
   def verses(_,_)
   end
 
   def song
+  end
+
+  def verse_for(n)
+    case
+    when n % 15 == 0
+      Verse.new(n, &FizzBuzz)
+    when n % 3 == 0
+      Verse.new(n, &Fizz)
+    when n % 5 == 0
+      Verse.new(n, &Buzz)
+    else
+      Verse.new(n, &Num)
+    end
+  end
+end
+
+class Verse
+  attr_reader :number
+
+  def initialize(number, &answer)
+    @number = number
+    @answer = answer
+  end
+
+  def text
+    @answer.call self
   end
 end
