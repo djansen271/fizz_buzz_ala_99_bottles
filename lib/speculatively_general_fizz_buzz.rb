58fcf574f7ec43e70163c0cc18ef15eb551bd2f4
Damon Jansen <djansen271@gmail.com>
2021-02-07 11:42:07 -0800

    Fix number result in SpecultivelyGeneralFizzBuzz; add verses & song

--- lib/speculatively_general_fizz_buzz.rb
+++ lib/speculatively_general_fizz_buzz.rb
@@ -2,16 +2,18 @@ class SpeculativelyGeneralFizzBuzz
   Fizz = lambda { |n| 'Fizz' }
   Buzz = lambda { |n| 'Buzz' }
   FizzBuzz = lambda { |n| 'FizzBuzz' }
-  Num = lambda { |n| n.to_s }
+  Num = lambda { |n| n.number.to_s }
 
   def single_verse(n)
     verse_for(n).text
   end
 
-  def verses(_,_)
+  def verses(starting,ending)
+    (starting..ending).map{ |n| single_verse(n) }
   end
 
   def song
+    verses(1,100)
   end
 
   def verse_for(n)
