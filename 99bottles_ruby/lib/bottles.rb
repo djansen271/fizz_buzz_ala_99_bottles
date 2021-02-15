7c0e0551263d5774d430ea25f7e7aede1fce520d
Damon Jansen <djansen271@gmail.com>
2021-02-15 13:24:59 -0800

    Call #container

--- 99bottles_ruby/lib/bottles.rb
+++ 99bottles_ruby/lib/bottles.rb
@@ -20,8 +20,8 @@ class Bottles
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
     when 2
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
+      "#{number} #{container} of beer on the wall, " +
+      "#{number} #{container} of beer.\n" +
       "Take one down and pass it around, " +
       "#{number - 1} bottle of beer on the wall.\n"
     else
