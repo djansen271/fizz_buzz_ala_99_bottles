c90c5442599bd386f06c0de46ff1245f747e126b
Damon Jansen <djansen271@gmail.com>
2021-02-15 13:21:14 -0800

    Use number in case statement

--- 99bottles_ruby/lib/bottles.rb
+++ 99bottles_ruby/lib/bottles.rb
@@ -21,7 +21,7 @@ class Bottles
       "no more bottles of beer on the wall.\n"
     when 2
       "#{number} bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
+      "#{number} bottles of beer.\n" +
       "Take one down and pass it around, " +
       "1 bottle of beer on the wall.\n"
     else
