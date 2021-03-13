97ed18023fff9039556dcd2f1ecb950335b51015
Damon Jansen <djansen271@gmail.com>
2021-03-13 11:19:44 -0800

    Add 99_bottles_js and first test passing

---
+++ 99bottles_js/lib/bottles.js
@@ -0,0 +1,14 @@
+// import { downTo } from './helpers';
+
+export class Bottles {
+  constructor() {
+  }
+
+  verse() {
+    let str =       '99 bottles of beer on the wall, ' +
+    '99 bottles of beer.\n' +
+    'Take one down and pass it around, ' +
+    '98 bottles of beer on the wall.\n';
+    return str
+  }
+}
