49484847e6ecf05854c57413e4bf5ae6acc798e9
Damon Jansen <djansen271@gmail.com>
2021-03-13 12:38:14 -0800

    Bottle js -- each #verse test passing

--- 99bottles_js/lib/bottles.js
+++ 99bottles_js/lib/bottles.js
@@ -13,6 +13,24 @@ export class Bottles {
           'Take one down and pass it around, ' +
           '2 bottles of beer on the wall.\n';
           break;
+      case 2:
+        str = '2 bottles of beer on the wall, ' +
+          '2 bottles of beer.\n' +
+          'Take one down and pass it around, ' +
+          '1 bottle of beer on the wall.\n';
+        break;
+      case 1:
+        str = '1 bottle of beer on the wall, ' +
+          '1 bottle of beer.\n' +
+          'Take it down and pass it around, ' +
+          'no more bottles of beer on the wall.\n';
+        break;
+      case 0:
+        str = 'No more bottles of beer on the wall, ' +
+        'no more bottles of beer.\n' +
+        'Go to the store and buy some more, ' +
+        '99 bottles of beer on the wall.\n';
+        break;
       case 99:
         str = '99 bottles of beer on the wall, ' +
           '99 bottles of beer.\n' +
