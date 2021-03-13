1faf52d2c0ea6ecc3a5b2caca0be1103b96b7edb
Damon Jansen <djansen271@gmail.com>
2021-03-13 10:42:24 -0800

    Make typescript project

---
+++ typescript_99_bottles/src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var world = 'world';
+function hello(word) {
+    if (word === void 0) { word = world; }
+    return "Hello " + world;
+}
+exports.hello = hello;
---
+++ typescript_99_bottles/src/index.ts
@@ -0,0 +1,4 @@
+const world = 'world';
+
+export function hello(word: string = world): string {
+  return `Hello ${world}`; }
