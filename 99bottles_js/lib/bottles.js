330387d2824b7dd3eabe6b5a5b3e7b1a5c97b438
Damon Jansen <djansen271@gmail.com>
2021-03-13 12:34:38 -0800

    bottles js - two tests passing

--- 99bottles_js/lib/bottles.js
+++ 99bottles_js/lib/bottles.js
@@ -4,11 +4,21 @@ export class Bottles {
   constructor() {
   }
 
-  verse() {
-    let str =       '99 bottles of beer on the wall, ' +
-    '99 bottles of beer.\n' +
-    'Take one down and pass it around, ' +
-    '98 bottles of beer on the wall.\n';
+  verse(number) {
+    let str
+    switch(number) {
+      case 3:
+        str = '3 bottles of beer on the wall, ' +
+          '3 bottles of beer.\n' +
+          'Take one down and pass it around, ' +
+          '2 bottles of beer on the wall.\n';
+          break;
+      case 99:
+        str = '99 bottles of beer on the wall, ' +
+          '99 bottles of beer.\n' +
+          'Take one down and pass it around, ' +
+          '98 bottles of beer on the wall.\n';
+    }
     return str
   }
 }
