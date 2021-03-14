181f5c02b0d82dce8ca5ca3dd7608bf1c1f1ba92
Damon Jansen <djansen271@gmail.com>
2021-03-14 10:32:44 -0700

    Remove index.js

--- typescript_99_bottles/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var world = 'world';
-function hello(word) {
-    if (word === void 0) { word = world; }
-    return "Hello " + world;
-}
-exports.hello = hello;
