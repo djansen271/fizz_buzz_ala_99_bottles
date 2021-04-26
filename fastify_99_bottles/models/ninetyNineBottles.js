1d61ecb88888f8d680f82b07fffe62fae283f7fa
Damon Jansen <djansen271@gmail.com>
2021-04-25 20:48:33 -0700

    return verse 1

--- fastify_99_bottles/models/ninetyNineBottles.js
+++ fastify_99_bottles/models/ninetyNineBottles.js
@@ -4,7 +4,22 @@ exports.NinetyNineBottles = void 0;
 var NinetyNineBottles = /** @class */ (function () {
     function NinetyNineBottles() {
     }
-    NinetyNineBottles.prototype.verse = function (n) { return "blah"; };
+    NinetyNineBottles.prototype.verse = function (n) {
+        var verse;
+        console.log("n: " + n);
+        switch (n) {
+            case 1: {
+                return '1 bottle of beer on the wall, ' +
+                    '1 bottle of beer.\n' +
+                    'Take it down and pass it around, ' +
+                    'no more bottles of beer on the wall.\n';
+            }
+            default: {
+                return "things";
+            }
+        }
+        return verse;
+    };
     NinetyNineBottles.prototype.verses = function (start, ending) {
         return "verses method";
     };
--- fastify_99_bottles/models/ninetyNineBottles.ts
+++ fastify_99_bottles/models/ninetyNineBottles.ts
@@ -1,7 +1,22 @@
 import { SongGenerator } from '../interfaces/interfaces';
 
 export class NinetyNineBottles implements SongGenerator {
-  verse(n: number): string { return "blah" }
+  verse(n: number): string {
+    let verse;
+    console.log(`n: ${n}`);
+    switch (n) {
+      case 1: {
+        return '1 bottle of beer on the wall, ' +
+        '1 bottle of beer.\n' +
+        'Take it down and pass it around, ' +
+        'no more bottles of beer on the wall.\n';
+      }
+      default: {
+          return "things";
+        }
+      }
+      return verse;
+    }
   verses(start: number, ending: number): string {
     return "verses method"
   }
