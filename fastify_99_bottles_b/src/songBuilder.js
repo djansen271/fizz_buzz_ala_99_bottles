8774987fc437b3b4cd0b4d2ff162d4e129c550bb
Damon Jansen <djansen271@gmail.com>
2021-05-02 16:44:23 -0700

    Add SongBuilder class

---
+++ fastify_99_bottles_b/src/songBuilder.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SongBuilder = void 0;
+var SongBuilder = /** @class */ (function () {
+    function SongBuilder() {
+    }
+    SongBuilder.prototype.helloWorld = function () {
+        return "hello world";
+    };
+    return SongBuilder;
+}());
+exports.SongBuilder = SongBuilder;
---
+++ fastify_99_bottles_b/src/songBuilder.ts
@@ -0,0 +1,5 @@
+export class SongBuilder {
+  helloWorld() {
+    return "hello world";
+  }
+}
