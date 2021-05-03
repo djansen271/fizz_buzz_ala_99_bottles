f5d0b8cca114921ff6f34c0c70d24ac44b391ece
Damon Jansen <djansen271@gmail.com>
2021-05-02 17:24:22 -0700

    verse 2  passing

--- fastify_99_bottles_b/src/songBuilder.spec.ts
+++ fastify_99_bottles_b/src/songBuilder.spec.ts
@@ -26,5 +26,13 @@ describe('verse', () => {
     'Take one down and pass it around, ' + '2 bottles of beer on the wall.\n';
     expect(songBuilder.verse(3)).toEqual(expected);
   })
+
+  test('verse 2', () => {
+    const expected =
+    '2 bottles of beer on the wall, ' +
+    '2 bottles of beer.\n' +
+    'Take one down and pass it around, ' + '1 bottle of beer on the wall.\n';
+    expect(songBuilder.verse(2)).toEqual(expected);
+  });
 })
 
--- fastify_99_bottles_b/src/songBuilder.ts
+++ fastify_99_bottles_b/src/songBuilder.ts
@@ -4,11 +4,17 @@ export class SongBuilder {
   }
 
   verse(n: number) {
-    const verse =
-      `${n} bottles of beer on the wall, ` +
-      `${n} bottles of beer.\n` +
-      `Take one down and pass it around, ` +
-      `${n - 1} bottles of beer on the wall.\n`;
-    return verse;
+    switch (n) {
+      case 2:
+        return '2 bottles of beer on the wall, ' +
+        '2 bottles of beer.\n' +
+        'Take one down and pass it around, ' +
+        '1 bottle of beer on the wall.\n';
+      default:
+        return `${n} bottles of beer on the wall, ` +
+        `${n} bottles of beer.\n` +
+        `Take one down and pass it around, ` +
+        `${n - 1} bottles of beer on the wall.\n`;
+    }
   }
 }
