3da1569d6de8e53cabf1c72260ed3b0467b207b1
Damon Jansen <djansen271@gmail.com>
2021-05-02 17:09:06 -0700

    get hello world test passing

--- fastify_99_bottles_b/src/songBuilder.spec.ts
+++ fastify_99_bottles_b/src/songBuilder.spec.ts
@@ -3,3 +3,8 @@ import { SongBuilder } from './songBuilder';
 test('2 is 2', () => {
   expect(true).toBe(true);
 })
+
+test('hello world', () => {
+  let songBuilder = new SongBuilder;
+  expect(songBuilder.helloWorld()).toEqual('hello world');
+})
