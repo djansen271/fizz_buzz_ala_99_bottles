8c32e414322643e16398560c5de6b77a4011ea09
Damon Jansen <djansen271@gmail.com>
2021-05-02 17:14:19 -0700

    verse 99 passing

--- fastify_99_bottles_b/src/songBuilder.spec.ts
+++ fastify_99_bottles_b/src/songBuilder.spec.ts
@@ -8,3 +8,15 @@ test('hello world', () => {
   let songBuilder = new SongBuilder;
   expect(songBuilder.helloWorld()).toEqual('hello world');
 })
+
+describe('verse', () => {
+  let songBuilder = new SongBuilder;
+
+  test('verse 99', () => {
+    const expected =
+'99 bottles of beer on the wall, ' + '99 bottles of beer.\n' +
+'Take one down and pass it around, ' + '98 bottles of beer on the wall.\n';
+    expect(songBuilder.verse(99)).toEqual(expected);
+  })
+})
+
--- fastify_99_bottles_b/src/songBuilder.ts
+++ fastify_99_bottles_b/src/songBuilder.ts
@@ -2,4 +2,11 @@ export class SongBuilder {
   helloWorld() {
     return "hello world";
   }
+
+  verse(n: number) {
+    const verse =
+      '99 bottles of beer on the wall, ' + '99 bottles of beer.\n' +
+      'Take one down and pass it around, ' + '98 bottles of beer on the wall.\n';
+    return verse;
+  }
 }
