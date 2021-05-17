319521a66ca035863c045ff354ccbe5974c7a55b
Damon Jansen <djansen271@gmail.com>
2021-05-17 11:46:21 -0700

    Add BottlesVerse tests for 6,7

--- 99bottles_ruby/spec/lib/bottles_verse_spec.rb
+++ 99bottles_ruby/spec/lib/bottles_verse_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe BottleVerse do
-  it 'renders the first verse' do
+  it 'renders the upper bound of the general rule' do
     expected =
       "99 bottles of beer on the wall, " +
       "99 bottles of beer.\n" +
@@ -12,7 +12,7 @@ describe BottleVerse do
   end
 
 
-  it 'renders another verse' do
+  it 'renders the lower bound of the general rule' do
     expected =
       "3 bottles of beer on the wall, " +
       "3 bottles of beer.\n" +
@@ -21,7 +21,21 @@ describe BottleVerse do
     expect(BottleVerse.lyrics(3)).to eq(expected)
   end
 
-  it 'renders verse with 2 botles' do
+  it 'renders the verse with 7 bottles' do
+    expected = "7 bottles of beer on the wall, " +
+      "7 bottles of beer.\n" +
+      "Take one down and pass it around, " + "1 six-pack of beer on the wall.\n"
+    expect(BottleVerse.lyrics(7)).to eq(expected)
+  end
+
+  it 'renders the verse with 6 bottles' do
+    expected =
+    "1 six-pack of beer on the wall, " + "1 six-pack of beer.\n" +
+    "Take one down and pass it around, " + "5 bottles of beer on the wall.\n"
+    expect(BottleVerse.lyrics(6)).to eq(expected)
+  end
+
+  it 'renders the verse with 2 bottles' do
     expected =
       "2 bottles of beer on the wall, " +
       "2 bottles of beer.\n" +
