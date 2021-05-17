2d3dd5d134c49ab94bc3ddf25994b4581133ede0
Damon Jansen <djansen271@gmail.com>
2021-05-17 11:38:44 -0700

    Move tests verse tests from Bottles to BottleVerse

--- 99bottles_ruby/spec/lib/bottles_spec.rb
+++ 99bottles_ruby/spec/lib/bottles_spec.rb
@@ -1,57 +1,6 @@
 require 'spec_helper'
 
 describe Bottles do
-  describe '#verse' do
-    it 'renders the first verse' do
-      expected =
-        "99 bottles of beer on the wall, " +
-        "99 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "98 bottles of beer on the wall.\n"
-
-      expect(subject.verse(99)).to eq(expected)
-    end
-
-    it 'renders another verse' do
-      expected =
-        "3 bottles of beer on the wall, " +
-        "3 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "2 bottles of beer on the wall.\n"
-      expect(subject.verse(3)).to eq(expected)
-    end
-
-    it 'renders verse with 2 botles' do
-      expected =
-        "2 bottles of beer on the wall, " +
-        "2 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "1 bottle of beer on the wall.\n"
-
-      expect(subject.verse(2)).to eq(expected)
-    end
-
-    it 'renders the verse with 1 bottle' do
-      expected =
-        "1 bottle of beer on the wall, " +
-        "1 bottle of beer.\n" +
-        "Take it down and pass it around, " +
-        "no more bottles of beer on the wall.\n"
-
-      expect(subject.verse(1)).to eq(expected)
-    end
-
-    it 'renders the verse with 0 bottles' do
-      expected =
-        "No more bottles of beer on the wall, " +
-        "no more bottles of beer.\n" +
-        "Go to the store and buy some more, " +
-        "99 bottles of beer on the wall.\n"
-
-      expect(subject.verse(0)).to eq(expected)
-    end
-  end
-
   describe '#verses' do
   it 'renders a couple verses' do
     expected =
---
+++ 99bottles_ruby/spec/lib/bottles_verse_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe BottleVerse do
+  it 'renders the first verse' do
+    expected =
+      "99 bottles of beer on the wall, " +
+      "99 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "98 bottles of beer on the wall.\n"
+
+    expect(BottleVerse.lyrics(99)).to eq(expected)
+  end
+
+
+  it 'renders another verse' do
+    expected =
+      "3 bottles of beer on the wall, " +
+      "3 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "2 bottles of beer on the wall.\n"
+    expect(BottleVerse.lyrics(3)).to eq(expected)
+  end
+
+  it 'renders verse with 2 botles' do
+    expected =
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n"
+
+    expect(BottleVerse.lyrics(2)).to eq(expected)
+  end
+
+  it 'renders the verse with 1 bottle' do
+    expected =
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
+
+    expect(BottleVerse.lyrics(1)).to eq(expected)
+  end
+
+  it 'renders the verse with 0 bottles' do
+    expected =
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
+
+    expect(BottleVerse.lyrics(0)).to eq(expected)
+  end
+
+end
