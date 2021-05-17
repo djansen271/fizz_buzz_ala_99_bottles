4c91d84a5642d69ae3754521fd06dbd40402e22d
Damon Jansen <djansen271@gmail.com>
2021-05-17 13:55:20 -0700

    Rename Bottles->CountdownSong; stub verse_template in tests; delete redundant test

--- 99bottles_ruby/spec/lib/bottles_spec.rb
+++ 99bottles_ruby/spec/lib/bottles_spec.rb
@@ -1,40 +1,22 @@
 require 'spec_helper'
 
-describe Bottles do
+describe CountdownSong do
   describe '#verses' do
-  it 'renders a couple verses' do
-    expected =
-      "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n" +
-      "\n" +
-      "98 bottles of beer on the wall, " +
-      "98 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "97 bottles of beer on the wall.\n"
+    let(:verse_template) { class_double(BottleVerse) }
+    let(:subject) { CountdownSong.new(verse_template: verse_template) }
 
-      expect(subject.verses(99,98)).to eq(expected)
-    end
+    it 'renders a couple verses' do
+      expect(verse_template).to receive(:lyrics).with(99).and_return 'This is verse 99'
+      expect(verse_template).to receive(:lyrics).with(98).and_return 'This is verse 98'
+      expect(verse_template).to receive(:lyrics).with(97).and_return 'This is verse 97'
 
-    it 'renders a few verses' do
       expected =
-        "2 bottles of beer on the wall, " +
-        "2 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "1 bottle of beer on the wall.\n" +
-        "\n" +
-        "1 bottle of beer on the wall, " +
-        "1 bottle of beer.\n" +
-        "Take it down and pass it around, " +
-        "no more bottles of beer on the wall.\n" +
-        "\n" +
-        "No more bottles of beer on the wall, " +
-        "no more bottles of beer.\n" +
-        "Go to the store and buy some more, " +
-        "99 bottles of beer on the wall.\n"
-      expect(subject.verses(2,0)).to eq(expected)
-    end
+        "This is verse 99\n" +
+        "This is verse 98\n" +
+        "This is verse 97"
+
+        expect(subject.verses(99,97)).to eq(expected)
+      end
   end
 
   describe '#song' do
