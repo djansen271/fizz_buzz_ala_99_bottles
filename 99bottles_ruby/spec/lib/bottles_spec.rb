1b2ff8091267c3c93f75ef69479b93f29afc45a2
Damon Jansen <djansen271@gmail.com>
2021-05-17 15:34:55 -0700

    Rearrange tests
    No functionality changed

--- 99bottles_ruby/spec/lib/bottles_spec.rb
+++ 99bottles_ruby/spec/lib/bottles_spec.rb
@@ -13,49 +13,29 @@ describe VerseFake do
 end
 
 describe CountdownSong do
-  describe '#verses' do
+  let(:max) { 1000 }
+  let(:min) { 0 }
+  let(:subject) { CountdownSong.new(verse_template: verse_template, max: max, min: min) }
+
+  describe 'using class_double of BottleVerse' do
     let(:verse_template) { class_double(BottleVerse) }
-    let(:subject) { CountdownSong.new(verse_template: verse_template) }
 
-    it 'renders a couple verses' do
-      expect(verse_template).to receive(:lyrics).with(99).and_return 'This is verse 99'
-      expect(verse_template).to receive(:lyrics).with(98).and_return 'This is verse 98'
-      expect(verse_template).to receive(:lyrics).with(97).and_return 'This is verse 97'
-
-      expected =
-        "This is verse 99\n" +
-        "This is verse 98\n" +
-        "This is verse 97"
-
-        expect(subject.verses(99,97)).to eq(expected)
-      end
-
-    describe 'using VerseFake ala sandi metz' do
-      let(:subject) { CountdownSong.new(verse_template: VerseFake) }
+    describe 'verses' do
       it 'renders a couple verses' do
+        expect(verse_template).to receive(:lyrics).with(99).and_return 'This is verse 99'
+        expect(verse_template).to receive(:lyrics).with(98).and_return 'This is verse 98'
+        expect(verse_template).to receive(:lyrics).with(97).and_return 'This is verse 97'
+
         expected =
-          "This is verse 99.\n" + "\n" +
-          "This is verse 98.\n" + "\n" +
-          "This is verse 97.\n"
-        expect(subject.verses(99,97)).to eq(expected)
-      end
-    end
-  end
+          "This is verse 99\n" +
+          "This is verse 98\n" +
+          "This is verse 97"
 
-  describe '#verse' do
-
-  describe 'using FakeVerse ala sandi metz' do
-    let(:subject) { CountdownSong.new(verse_template: VerseFake) }
-      it 'renders a verse' do
-        expected = "This is verse 500.\n"
-
-        expect(subject.verse(500)).to eq(expected)
+          expect(subject.verses(99,97)).to eq(expected)
       end
     end
 
-    describe 'using a class double of BottleVerse' do
-      let(:verse_template) { class_double(BottleVerse) }
-      let(:subject) { CountdownSong.new(verse_template: verse_template) }
+    describe 'verse' do
       it 'renders a verse' do
         expected = "This is verse 501."
 
@@ -65,19 +45,38 @@ describe CountdownSong do
     end
   end
 
+  describe 'using VerseFake ala Sandi Metz' do
+    let(:subject) { CountdownSong.new(verse_template: VerseFake) }
 
+    describe 'verses' do
+      it 'renders a couple verses' do
+        expected =
+          "This is verse 99.\n" + "\n" +
+          "This is verse 98.\n" + "\n" +
+          "This is verse 97.\n"
+        expect(subject.verses(99,97)).to eq(expected)
+      end
+    end
 
+    describe 'verse' do
+      it 'renders a verse' do
+        expected = "This is verse 500.\n"
 
-  describe '#song' do
-    let(:subject) { CountdownSong.new(verse_template: VerseFake, max: 47, min: 43) }
-    it 'renders the entire song' do
-      expected =
-        "This is verse 47.\n" + "\n" +
-        "This is verse 46.\n" + "\n" +
-        "This is verse 45.\n" + "\n" +
-        "This is verse 44.\n" + "\n" +
-        "This is verse 43.\n"
-      expect(subject.song).to eq(expected)
+        expect(subject.verse(500)).to eq(expected)
+      end
+    end
+
+    describe '#song' do
+      let(:subject) { CountdownSong.new(verse_template: VerseFake, max: 47, min: 43) }
+      it 'renders the entire song' do
+        expected =
+          "This is verse 47.\n" + "\n" +
+          "This is verse 46.\n" + "\n" +
+          "This is verse 45.\n" + "\n" +
+          "This is verse 44.\n" + "\n" +
+          "This is verse 43.\n"
+        expect(subject.song).to eq(expected)
+      end
     end
   end
 end
